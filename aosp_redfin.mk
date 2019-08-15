1c2dbd8222ff7e51bf344765acd252859fbf09f7
Alex Hong <rurumihong@google.com>
2019-08-16 01:22:57 +0800

    Create aosp_redfin target
    Bug: 139478768
    Change-Id: I061f89a72b5e7b81280406abf85cca479b86f340

---
+++ aosp_redfin.mk
@@ -0,0 +1,47 @@
+#
+# Copyright 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/mainline.mk)
+
+$(call inherit-product, device/google/redfin/device-redfin.mk)
+$(call inherit-product-if-exists, vendor/google_devices/redfin/proprietary/device-vendor.mk)
+$(call inherit-product-if-exists, vendor/google_devices/redfin/prebuilts/device-vendor-redfin.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Ring_Synth_04.ogg \
+    ro.com.android.dataroaming=true \
+
+PRODUCT_PACKAGES += \
+    PhotoTable \
+    WallpaperPicker \
+    WAPPushManager \
+
+# Don't build super.img.
+PRODUCT_BUILD_SUPER_PARTITION := false
+
+# b/113232673 STOPSHIP deal with Qualcomm stuff later
+# PRODUCT_RESTRICT_VENDOR_FILES := all
+
+PRODUCT_MANUFACTURER := Google
+PRODUCT_BRAND := Android
+PRODUCT_NAME := aosp_redfin
+PRODUCT_DEVICE := redfin
+PRODUCT_MODEL := AOSP on redfin
+
+PRODUCT_COPY_FILES += \
+    device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
