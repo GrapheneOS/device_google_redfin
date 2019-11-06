c260ea6caee9068a852761fa00a749c6b70e44e6
Tianjie Xu <xunchang@google.com>
2019-11-06 15:15:32 -0800

    Unify the recovery UI for pixel 3+
    The Pixel 3 and 4 are using the identical RecoveryUI class. So
    consolidate them into the pixel common directory. This will ease the
    work to unify the vendor space /misc access later.
    
    Bug: 144810561
    Test: wipe data in bramble and redfin
    Change-Id: I43b7d04fe65ce919eb5c81a39034f50ce2ca9626
    Signed-off-by: Chenglu Lin <chenglulin@google.com>

--- redfin/BoardConfig.mk
+++ redfin/BoardConfig.mk
@@ -31,11 +31,6 @@ TARGET_BOARD_COMMON_PATH := device/google/redfin/sm7250
 # DTBO partition definitions
 BOARD_PREBUILT_DTBOIMAGE := device/google/redfin-kernel/dtbo.img
 
-TARGET_RECOVERY_UI_LIB := \
-  librecovery_ui_redfin \
-  libnos_citadel_for_recovery \
-  libnos_for_recovery
-
 TARGET_FS_CONFIG_GEN := device/google/redfin/config.fs
 
 # Kernel modules
