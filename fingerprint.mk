1c489b372e23f26b5b6f226aa3d778b375b29465
eddielan <eddielan@google.com>
2019-11-15 14:36:34 +0800

    Fingerprint: Add fingerprint frameowkr permisson file (Fix BB)
    Bug: 144473297
    Test: Fingerprint icon appears in Setting UI.
    Change-Id: I331fec4c8fa597c0815b62b46817ed239a5d5e85

--- fingerprint.mk
+++ fingerprint.mk
@@ -6,4 +6,5 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
-    $(LOCAL_PATH)/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc
+    $(LOCAL_PATH)/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
