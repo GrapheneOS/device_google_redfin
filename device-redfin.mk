a2f84f31aabe9f259f8b1d5047d56e1a82e091ce
tangrobin <tangrobin@google.com>
2020-03-03 16:45:21 +0800

    Redfin: Move the high fps early phase offset to device-common.mk
    In Android R, the SurfaceFlinger calculate phase offsets
    based on duration. Thus, this does not need to define
    specific phase offsets for high refresh rate.
    
    Bug: 143933518
    Bug: 142222407
    Test: scrolling, app launching/closing, playing video
    Change-Id: I5d07af4b505a205060885ac90b4271a8441ffde5

--- device-redfin.mk
+++ device-redfin.mk
@@ -179,16 +179,5 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_touch_timer_ms=200
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_display_power_timer_ms=1000
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
-# High fps sf early and early gl phase offset
-# The default sf phase offset is set to 6ms, to avoid it be included into next
-# vsync threshold, set high fps early sf and next vsync threshold phase offset
-# to 6.1ms, which is bigger than all sf phase offsets in normal frame rate.
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.high_fps_early_phase_offset_ns=6100000
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.high_fps_early_gl_phase_offset_ns=9000000
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.phase_offset_threshold_for_next_vsync_ns=6100000
-
 # Keyboard height ratio
 PRODUCT_PRODUCT_PROPERTIES += ro.com.google.ime.height_ratio=1.2
