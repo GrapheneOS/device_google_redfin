c4b2f6edfe065996666537ae3f58152abff608c9
Jack Wu <wjack@google.com>
2020-03-12 11:35:50 +0800

    health: move health hal 2.1 to redbull
    Bug: 145943771
    Bug: 150288173
    Test: lshal debug android.hardware.health@2.1::IHealth/default
    Signed-off-by: Jack Wu <wjack@google.com>
    Change-Id: I1856d3cc6427c3bcdbab28b039e9dc30dc45d9e4

--- device-redfin.mk
+++ device-redfin.mk
@@ -115,10 +115,6 @@ endif
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.2-service.redfin
 
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-redfin \
-    android.hardware.health@2.1-service
-
 # Vibrator HAL
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.3-service.redfin
