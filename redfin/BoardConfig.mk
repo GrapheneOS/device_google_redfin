f97c99c34483d01ea791b61eae00d4b78dffd9d5
yihsiangpeng <yihsiangpeng@google.com>
2021-03-29 17:16:54 +0800

    use vintf_fragments instead of adding it to device-specific manifests
    Bug: 183536544
    Signed-off-by: yihsiangpeng <yihsiangpeng@google.com>
    Change-Id: I433aaeed7dce96e33f1a4b466039b8d9614a1b18

--- redfin/BoardConfig.mk
+++ redfin/BoardConfig.mk
@@ -23,7 +23,6 @@ else
   USES_DEVICE_GOOGLE_REDFIN := true
 
   include device/google/redbull/BoardConfig-common.mk
-  DEVICE_MANIFEST_FILE += device/google/redfin/manifest.xml
   DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/redfin/device_framework_matrix_product.xml
 
   # Testing related defines
