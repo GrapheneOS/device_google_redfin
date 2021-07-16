f7f40beae5a41662543ad7c79be1a3a94d7d4f79
Daniel Micay <danielmicay@gmail.com>
2021-07-15 23:55:44 -0400

    remove aosp_ prefix from product name

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,11 +15,11 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_redfin.mk \
+    $(LOCAL_DIR)/redfin.mk \
     $(LOCAL_DIR)/aosp_redfin_64.mk \
     $(LOCAL_DIR)/vf/aosp_redfin_vf.mk \
     $(LOCAL_DIR)/aosp_redfin_hwasan.mk \
 
 COMMON_LUNCH_CHOICES := \
-    aosp_redfin-userdebug \
+    redfin-userdebug \
     aosp_redfin_vf-userdebug \
