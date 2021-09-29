5b0a254b12c242cb26049aa63389a1186d38b1b5
flawedworld <flawedworld@flawed.world>
2021-09-29 04:27:43 +0100

    Use APNs and CarrierConfig from APV

--- redfin.mk
+++ redfin.mk
@@ -3,3 +3,5 @@ $(call inherit-product, device/google/redfin/aosp_redfin.mk)
 
 # Remove AOSP prefix from product name
 PRODUCT_NAME := redfin
+# Tell build system not to bundle sample APNs from AOSP
+OEM_APNS := true
